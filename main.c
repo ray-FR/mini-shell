3e24a56a1d9b7e39f7d1c3ceba1f378bd65c4709
R.I <ares1345istaken@gmail.com>
2026-02-17 14:06:50 +0100

    Import getfulllogin from previous work

--- main.c
+++ main.c
@@ -1,3 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+char* getfulllogin(void){
+    char* name;
+    char host[256];
+    char cwd[256];
+
+    if ((name = getlogin()) == NULL){
+        perror("name");
+        exit(1);
+    }
+    if (gethostname(host, 256) < 0){
+        perror("gethostname");
+        exit(2);
+    }
+    if (getcwd(cwd, 256) < 0){
+        perror("getcwd");
+        exit(3);
+    }
+    
+    strcat(name, "@");
+    strcat(name, host);
+    strcat(name, ":");
+    strcat(name, cwd);
+    fprintf(stderr, "%s\n", name);
+    
+    return name;
+}
