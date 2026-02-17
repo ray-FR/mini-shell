91e0c0f0068afeb48858ff989219470b59ca9b53
R.I <ares1345istaken@gmail.com>
2026-02-17 22:50:02 +0100

    Import code from teacher

---
+++ command.c
@@ -0,0 +1,38 @@
+#include "command.h"
+
+int count_args(const char *s)
+{
+    int n = 0;
+    char p = ' ';
+    while (*s) {
+        if (isspace(p) && !isspace(*s)) n++;
+        p = *s++;
+    }
+    return n;
+}
+
+struct command * parse_cmd(const char *buf, int len)
+{
+    struct command *cmd = NULL;
+    char prev = ' ';
+    int n, beg = 0;
+
+    if ((n = count_args(buf)) == 0) return NULL;
+
+    cmd = malloc(sizeof(*cmd));
+    cmd->argc = 0;
+    cmd->argv = malloc((n + 1) * sizeof(*cmd->argv));
+
+    for (int i = 0; i < len; i++) {
+        if (!isspace(buf[i]) && isspace(prev)) { 
+            beg = i; 
+        }
+        else if (isspace(buf[i]) && !isspace(prev)) {
+            cmd->argv[cmd->argc++] = strndup(buf + beg, i - beg);
+        }
+        prev = buf[i];
+    }
+
+    cmd->argv[cmd->argc] = NULL;
+    return cmd;
+}
