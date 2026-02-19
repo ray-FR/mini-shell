fd929e89816ef6618b363424a2d2e9d7869912ab
R.I <ares1345istaken@gmail.com>
2026-02-19 13:05:49 +0100

    finish exec_cmd v1

--- exec.c
+++ exec.c
@@ -4,4 +4,19 @@
 
 int exec_cmd(struct command* cmd){
     pid_t pid;
+
+    pid = fork();
+    if (pid < 0){
+        perror("fork");
+        exit(1);
+    }
+    if (pid == 0){
+        int errcode = execvp(cmd->argv[0], &(cmd->argv[0]));
+        perror("execvp");
+        return errcode;
+    } else 
+        wait(NULL);
+    return 0;
+
+    
 }
