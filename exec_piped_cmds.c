6ef7226528f09b18565d5ed462ab35c1ae515fc8
R.I <ares1345istaken@gmail.com>
2026-02-21 10:16:01 +0100

    Add pid_t's to store the pids && begin implementing first child processor

--- exec_piped_cmds.c
+++ exec_piped_cmds.c
@@ -4,11 +4,20 @@
 #include "command.h"
 
 void exec_piped_cmds(struct command* cmd1, struct command* cmd2){
+    pid_t pid1;
+    pid_t pid2;
+
     int fd[2] = {1, 0};
     if (pipe(fd) < 0){
         perror("pipe");
         exit(1);
     }
-
+    if ((pid1 = fork()) < 0){
+        perror("fork1");
+        exit(1);
+    }
+    if (pid1 == 0){
+        
+    }
 
 }
