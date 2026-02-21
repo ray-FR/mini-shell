50690a57e7fa3ad6cc251d1d890f5ee66a274847
R.I <ares1345istaken@gmail.com>
2026-02-21 10:09:15 +0100

    Trying to implement pipe

--- exec_piped_cmds.c
+++ exec_piped_cmds.c
@@ -4,6 +4,11 @@
 #include "command.h"
 
 void exec_piped_cmds(struct command* cmd1, struct command* cmd2){
+    int fd[2] = {1, 0};
+    if (pipe(fd) < 0){
+        perror("pipe");
+        exit(1);
+    }
 
-    
+
 }
