8c8b062297a3a709c918fddb67161e8ab72f3107
R.I <ares1345istaken@gmail.com>
2026-02-21 10:23:25 +0100

    Working on implementing first child process

--- exec_piped_cmds.c
+++ exec_piped_cmds.c
@@ -6,6 +6,8 @@
 void exec_piped_cmds(struct command* cmd1, struct command* cmd2){
     pid_t pid1;
     pid_t pid2;
+    int fdexec1;
+    int fdexec2;
 
     int fd[2] = {1, 0};
     if (pipe(fd) < 0){
@@ -17,7 +19,13 @@ void exec_piped_cmds(struct command* cmd1, struct command* cmd2){
         exit(1);
     }
     if (pid1 == 0){
+        if ((fdexec1 = dup2(0, 1)) < 0){
+            perror("fdexec1");
+            exit(1);
+        }
+        close(0);
         
+
     }
 
 }
