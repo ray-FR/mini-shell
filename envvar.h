92f7d1e172bc2af2e8c65d19d1c6203fc25ddf11
R.I <ares1345istaken@gmail.com>
2026-02-25 12:39:41 +0100

    Modifs to envvar.h

--- envvar.h
+++ envvar.h
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 struct EVAR {
-    char* ENVVAR;
-    char* value;
+    char ENVVAR[80];
+    char value[512];
 };
 
 struct envVar_t {
     int nbVar;
-    struct EVAR* vars;
+    struct EVAR** vars;
 };
 
 
 struct envVar_t * initEnvVar();
-struct envVar_t* freeEnvVar();
+void freeEnvVar(struct envVar_t* EV);
