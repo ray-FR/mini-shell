118660228c931d866590793f098992b2e202edd0
R.I <ares1345istaken@gmail.com>
2026-02-25 12:41:46 +0100

    Fix bug in envvar.c

--- envvar.c
+++ envvar.c
@@ -3,5 +3,25 @@
 struct envVar_t * initEnvVar(){
     struct envVar_t* EV = NULL;
     EV = malloc(sizeof(*EV));
-    EV->nbVar = 0;
+    EV->nbVar = 2;
+    EV->vars = malloc(EV->nbVar * sizeof(*EV->vars));
+    for(int i =0;i<2;i++)
+        EV->vars[i] = malloc(sizeof(*EV->vars[i]));
+
+    strcpy(EV->vars[0]->ENVVAR, "$PWD");
+    if (getcwd(EV->vars[0]->value, 512) == NULL){perror("getcwd"); exit(1);}
+
+    strcpy(EV->vars[1]->ENVVAR, "$USER");
+    char* ec = getlogin();
+    (strcpy(EV->vars[1]->value, ec));
+    //free(ec);
+    return EV;
+}
+
+void freeEnvVar(struct envVar_t* EV){
+    for (int i = 0; i<EV->nbVar; i++ ){
+        free(EV->vars[i]);
+    }
+    free(EV->vars);
+    free(EV);
 }
