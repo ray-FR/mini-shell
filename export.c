1719f10d0c369708eb5a33f376b7252d3a032ae1
R.I <ares1345istaken@gmail.com>
2026-02-25 16:02:16 +0100

    Finish implementing V1 of builtin_export

--- export.c
+++ export.c
@@ -2,7 +2,8 @@
 #include "envvar.h"
 
 int builtin_export(struct command* cmd, struct envVar_t* EV){
-    if(cmd->argc > 2){
+    char* varName = "$";
+    if(cmd->argc > 4){
         printf("export: too many arguments\n");
         return 1;
     }
@@ -10,6 +11,23 @@ int builtin_export(struct command* cmd, struct envVar_t* EV){
         printf("usage: export VARIABLE_NAME = <value>\n");
         return 1;
     }
+    if(cmd->argc == 4){
+        if(cmd->argv[1][0] != '$') strcat(varName, cmd->argv[1]);
+        else varName = strdup(cmd->argv[1]);
+        EV->nbVar++;
+        strcpy(EV->vars[EV->nbVar - 1]->ENVVAR, varName);
+        strcpy(EV->vars[EV->nbVar - 1]->ENVVAR, cmd->argv[4]);
+        return 0;
 
+    }
     
+    if(cmd->argv[1][0] != '$') strcat(varName, cmd->argv[1]);
+    else varName = strdup(cmd->argv[1]);
+    EV->nbVar++;
+    strcpy(EV->vars[EV->nbVar - 1]->ENVVAR, varName);
+    strcpy(EV->vars[EV->nbVar - 1]->ENVVAR, cmd->argv[2]);
+
+
+    return 0;
+
 }
