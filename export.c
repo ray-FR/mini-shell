08cb6e82f57c4f59ba7dfa44e5171ea3cb8d65a6
R.I <ares1345istaken@gmail.com>
2026-02-25 15:29:18 +0100

    Add verification on arg count

--- export.c
+++ export.c
@@ -2,5 +2,14 @@
 #include "envvar.h"
 
 int builtin_export(struct command* cmd, struct envVar_t* EV){
+    if(cmd->argc > 2){
+        printf("export: too many arguments\n");
+        return 1;
+    }
+    else if (cmd->argc < 2){
+        printf("usage: export VARIABLE_NAME = <value>\n");
+        return 1;
+    }
+
     
 }
