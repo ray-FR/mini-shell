d46def6c1198ca78c5bc6c37548a105ebd6e85ca
R.I <ares1345istaken@gmail.com>
2026-02-27 16:04:28 +0100

    Begin implementing '>' redirections

--- src/redirect.c
+++ src/redirect.c
@@ -1,5 +1,13 @@
+#include <fcntl.h>
 #include "command.h"
 
 void builtin_redirect(struct command* cmd, int typeOfRedirect){
-    
+    int fd, fd2;
+    switch (typeOfRedirect){
+        case 0:
+            if ((fd = open(cmd->argv[0], O_RDONLY)) < 0){
+                
+            }
+            break;
+    }
 }
