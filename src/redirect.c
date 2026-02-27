f8d04862a1b1a23abedabd57a41559b1ae1ffd0b
R.I <ares1345istaken@gmail.com>
2026-02-27 23:21:03 +0100

    Implement major part of the builtin_redirect function

--- src/redirect.c
+++ src/redirect.c
@@ -1,18 +1,27 @@
 #include <fcntl.h>
 #include "command.h"
 
-void builtin_redirect(struct command* cmd, int typeOfRedirect){
-    int fd, fd2;
+void builtin_redirect(struct command* cmd){
+    int fd;
     pid_t pid;
-    switch (typeOfRedirect){
-        case 0:
-            if ((fd = open(cmd->argv[2], O_WRONLY)) < 0){
-                perror('fd');
-                exit(1);
-            }
-
-            
-            break;
+    printf("%s\n", cmd->argv[3]);
+    if ((fd = open(cmd->argv[3], O_WRONLY)) < 0){
+        perror("fd");
+        exit(1);
+    }
+    if ((pid = fork()) < 0){
+        perror("fork");
+        exit(1);
+    }
+    if (pid == 0){
+        dup2(fd, 1);
+        close(fd);
+        execvp(cmd->argv[0], &cmd->argv[0]);
+    }
+    else{
+        close(fd);
+        waitpid(pid, NULL, 0);
+        printf("woo the bullshit worked\n");
     }
 }
 
