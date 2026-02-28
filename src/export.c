8c2de8805b4736384f8ea5e5723afaff5e8b0eba
R.I <ares1345istaken@gmail.com>
2026-02-28 16:51:36 +0100

    Other miscellaneous fixes

--- src/export.c
+++ src/export.c
@@ -8,6 +8,15 @@ int builtin_export(struct command* cmd, struct envVar_t* EV){
         printf("usage: export VARIABLE_NAME = <value>\n");
         return 1;
     }
+    else if (cmd->argc == 3){
+        if(cmd->argv[1][0] == '$') strcpy(varName, cmd->argv[1]);
+        else strcat(varName, cmd->argv[1]);
+        EV->nbVar++;
+        strcpy(EV->vars[EV->nbVar - 1]->ENVVAR, varName);
+        strcpy(EV->vars[EV->nbVar - 1]->ENVVAR, cmd->argv[2]);
+        return 0;
+
+    }
     else if(cmd->argc > 4 || (strcmp(cmd->argv[2], "=") != 0)){
         printf("export: too many arguments\n");
         return 1;
@@ -21,15 +30,7 @@ int builtin_export(struct command* cmd, struct envVar_t* EV){
         return 0;
 
     }
-    else if (cmd->argc == 3){
-        if(cmd->argv[1][0] == '$') strcpy(varName, cmd->argv[1]);
-        else strcat(varName, cmd->argv[1]);
-        EV->nbVar++;
-        strcpy(EV->vars[EV->nbVar - 1]->ENVVAR, varName);
-        strcpy(EV->vars[EV->nbVar - 1]->ENVVAR, cmd->argv[2]);
-        return 0;
-
-    }
+    
     printf("builtin_export: error\n");
     return 1;
 
