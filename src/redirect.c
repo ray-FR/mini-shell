c6b305fc2961d7e154b158599e1a8def3e319af6
R.I <ares1345istaken@gmail.com>
2026-02-28 12:42:27 +0100

    Implement '>' redirection + some change on how to manage the type of redirection

--- src/redirect.c
+++ src/redirect.c
@@ -1,19 +1,26 @@
 #include <fcntl.h>
 #include "command.h"
 
-void builtin_redirect(struct command* cmd){
+
+
+void builtin_redirect(struct command* cmd, char* fileName, int typeOfRedirection){
     int fd;
     pid_t pid;
-    printf("%s\n", cmd->argv[3]);
-    if ((fd = open(cmd->argv[3], O_WRONLY)) < 0){
+    
+    if ((fd = open(fileName, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR)) < 0){
         perror("fd");
         exit(1);
     }
+    
+    
     if ((pid = fork()) < 0){
         perror("fork");
         exit(1);
     }
     if (pid == 0){
+        if (ftruncate(fd, 0) == 0) 
+            lseek(fd, 0, SEEK_SET); // reposition to start for subsequent writes
+        
         dup2(fd, 1);
         close(fd);
         execvp(cmd->argv[0], &cmd->argv[0]);
